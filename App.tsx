6981d239e7906659bcec123ea118e7de5c7ea578
David <romao.carlosdavi@gmail.com>
2021-04-20 11:54:10 -0300

    segundo-dia

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
-import { Welcome } from './src/pages/Welcome'
-
-
+import Load from 'expo-app-loading'
+import { Useridentification } from './src/pages/Useidentification'
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+  if (!fontsLoaded)
+    return <Load />
+
+
+
   return (
-    <Welcome />)
+    <Useridentification />)
+
 }
---
+++ src/pages/Useidentification.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, View, Text } from 'react-native'
+
+export function Useridentification() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+                <View style={styles.form}>
+                    <Text>
+                        😁
+                    </Text>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+
+    }
+})
